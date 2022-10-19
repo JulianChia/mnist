b97e1679d9b0d1050d7348678724a8643a4c2045
JulianChia <julianchiayh@gmail.com>
2022-10-19 20:44:21 +0800

    Updated doc-string.

--- mnist.py
+++ mnist.py
@@ -60,8 +60,10 @@ def load_MNIST(path=None, normalise=True, flatten=True, onehot=True):
                  Create if nonexistant. Download any missing MNIST files.
      normalise - boolean: yes -> pixels RGB values [0,255] divided by 255.
                           no  -> pixels RGB values [0,255].
-     flatten   - boolean: yes -> pixels of each image stored as 1D numpy array.
-                          no  -> pixels of each image stored as 2D numpy array.
+     flatten   - boolean: yes -> pixels of all images stored as 2D numpy
+     array.
+                          no  -> pixels of all images stored as 3D numpy
+                          array.
      onehot    - boolean: yes -> labels stored as one-hot encoded numpy array.
                           no  -> labels values used.
 
